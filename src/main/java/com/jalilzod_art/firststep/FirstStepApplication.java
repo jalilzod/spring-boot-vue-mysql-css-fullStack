e7db1aed0d5fbff39df3ed203b141d473a1f74ee
jalilzod_art <mingyuan_12@qq.com>
2025-05-29 16:04:20 +0800

    init project

---
+++ src/main/java/com/jalilzod_art/firststep/FirstStepApplication.java
@@ -0,0 +1,13 @@
+package com.jalilzod_art.firststep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstStepApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstStepApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jalilzod_art/firststep/FirstStepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jalilzod_art.firststep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstStepApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
